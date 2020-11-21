b5c8cb95232c805bde1b1125e0334a5236d7d131
jcbackenddev <jcbackenddev@gmail.com>
2020-11-21 22:37:52 +0100

    Footer ready!

--- src/main/webapp/app/core/jhi-footer/jhi-footer.vue
+++ src/main/webapp/app/core/jhi-footer/jhi-footer.vue
@@ -1,66 +1,58 @@
 <template>
     <div id="footer" class="footer">
         <div id="footer-section">
-  <div id="footer-copy" class="content-safe">
-    
-    <div class="footer-group footer-group-logo">
-      <div id="footer-logo"></div>
-      <div id="footer-join-our-team" onclick="javascript:alert('Coming soon');"></div>
-      <div id="footer-facebook" class="footer-social-media-link" onclick="window.open('https://www.facebook.com/pages/Bruce-Heating-Air-Conditioning/153829408062627', '_blank');"></div>
-      <div id="footer-twitter" class="footer-social-media-link" onclick="window.open('https://twitter.com/BruceIncHVAC', '_blank');"></div>
-      <div id="footer-blogspot" class="footer-social-media-link" onclick="window.open('https://richlandpascokennewickhvac.blogspot.com', '_blank');"></div>
-    </div>
-    <div class="footer-group">
-      <h3>Residential</h3>
-      <a href="https://s.codepen.io/brianrak/debug/wBopQJ">Overview</a>
-      <a href="https://s.codepen.io/brianrak/debug/wBopQJ#maintenance">Maintenance Services</a>
-      <a href="https://s.codepen.io/brianrak/debug/wBopQJ#installation">Installation Services</a>
-      <a href="https://s.codepen.io/brianrak/debug/yNJyLm">Success Stories</a>
-    </div>
-    <div class="footer-group">
-      <h3>Commercial</h3>
-      <a href="https://s.codepen.io/brianrak/debug/GgWYgR">Overview</a>
-      <a href="https://s.codepen.io/brianrak/debug/GgWYgR#maintenance">Maintenance Services</a>
-      <a href="https://s.codepen.io/brianrak/debug/GgWYgR#construction">Construction Services</a>
-      <a href="https://s.codepen.io/brianrak/debug/gbmBLa">Projects</a>
-      <a href="https://s.codepen.io/brianrak/debug/bNWbZo">Success Stories</a>
-    </div>
-    <div class="footer-group">
-      <h3>Company Info</h3>
-      <a href="https://s.codepen.io/brianrak/debug/vOKENp">Overview</a>
-      <a href="https://s.codepen.io/brianrak/debug/vOKENp#about">About Us</a>
-      <a href="https://s.codepen.io/brianrak/debug/vOKENp#contact">Contact Us</a>
-      <a href="https://s.codepen.io/brianrak/debug/vOKENp#team">Our Team</a>
-    </div> 
-    <div class="clear-floats"></div>
-    
-    <div id="footer-contact">
-      <div>
-        <span class="nw footer-contact-company-name">Bruce Inc. Heating and Air Conditioning</span>
-        <span class="footer-divider">|</span>
-        <span class="nw">5115 W. Brinkley Rd., Ste. C</span>
-        <span class="footer-divider">|</span>
-        <span class="nw">Kennewick, WA 99338</span>
-      </div>
-      <div><span class="footer-divider">|</span></div>
-      <div>
-        <span class="nw">Phone: <a href="tel:509-734-0669">509-734-0669</a></span>
-        <span class="footer-divider">|</span>
-        <span class="nw">Fax: 509-783-7090</span>
-        <span class="footer-divider">|</span>
-        <span class="nw">Contractor #: BRUCEMI951LB</span>
-      </div>
-    </div>
-    
-    <div id="footer-copyright">Base content copyright 2014 Lennox Industries. All rights reserved. Additional content Copyright 2014 Bruce Inc. Heating and Air Conditioning. Duplication in part or in while is strictly prohibited.</div>
-  </div>
-</div>
 
-<!-- Preloads, super old-school -->
-<div style="display:none;">
-  <img src="http://www.braksoftware.com/codepen/bruce/v1/join-our-team-button-over-large.png" />
-  <img src="http://www.braksoftware.com/codepen/bruce/v1/join-our-team-button-over-medium.png" />
-</div>
+            <div id="footer-links">
+                <div class="footer-group">
+                    <h3>Residential</h3>
+                    <a href="">Overview</a>
+                    <a href="">Maintenance Services</a>
+                    <a href="">Installation Services</a>
+                    <a href="">Success Stories</a>
+                </div>
+                <div class="footer-group">
+                    <h3>Commercial</h3>
+                    <a href="">Overview</a>
+                    <a href="">Maintenance Services</a>
+                    <a href="">Construction Services</a>
+                    <a href="">Projects</a>
+                    <a href="">Success Stories</a>
+                </div>
+                <div class="footer-group">
+                    <h3>Company Info</h3>
+                    <a href="">Overview</a>
+                    <a href="">About Us</a>
+                    <a href="">Contact Us</a>
+                    <a href="">Our Team</a>
+                </div> 
+            </div>
+            
+            <div id="footer-text">
+                <div id="footer-contact">
+                    <div>
+                        <span class="nw footer-contact-company-name">Akroma Coorporation. Solutions for a better world</span>
+                        <span class="footer-divider">|</span>
+                        <span class="nw">7755 W. Better Rd., Ste. C</span>
+                        <span class="footer-divider">|</span>
+                        <span class="nw">Yaoundé, CMR 22557</span>
+                    </div>
+                    <div><span class="footer-divider">|</span></div>
+                    <div>
+                        <span class="nw">Phone: <a href="tel:554-754-0666">554-754-0666</a></span>
+                        <span class="footer-divider">|</span>
+                        <span class="nw">Fax: 554-754-0999</span>
+                        <span class="footer-divider">|</span>
+                        <span class="nw">Contractor #: AKROMA-CORP</span>
+                    </div>
+                </div>
+                
+                <div id="footer-copyright">Base content copyright 2020 Akroma & Truth Coorporation. 
+                    All rights reserved. Additional content Copyright 2020 Liz Inc. Solutions for a better world. 
+                    Duplication in part or in while is strictly prohibited.
+                </div>
+            </div>
+
+        </div>
     </div>
 </template>
 
@@ -68,12 +60,14 @@
 </script>
 
 <style scoped>
-    #footer-section {
+
+#footer-section {
   padding:40px 0px 40px 0px;
 }
-
-#footer-copy {
-  
+#footer-links {
+    width: 100%;
+    display: flex;
+    justify-content: center;
 }
 
 #footer-section h3 {
@@ -98,60 +92,22 @@
 }
 
 .footer-group {
-  float:left;
+  display: inline-block;
   margin:0px 50px 0px 0px;
 }
 
-#footer-logo {
-  background-image:url(http://www.braksoftware.com/codepen/bruce/v1/bruce-logo-footer-large.png);
-  background-size:100% 100%;
-  background-repeat:no-repeat;
-  width:199px;
-  height:46px;
-  margin:0px 60px 50px 0px;
-}
-
-#footer-join-our-team {
-  cursor:pointer;
-  background-image:url(http://www.braksoftware.com/codepen/bruce/v1/join-our-team-button-large.png);
-  background-size:100% 100%;
-  background-repeat:no-repeat;
-  width:184px;
-  height:41px;
-  margin:0px 0px 10px 0px;
-}
-
-#footer-join-our-team:hover {
-  background-image:url(http://www.braksoftware.com/codepen/bruce/v1/join-our-team-button-over-large.png);
-}
-
-.footer-social-media-link {
-  display:inline-block;
-  cursor:pointer;
-  background-position:center;
-  background-repeat:no-repeat;
-  width:27px;
-  height:26px;
-  margin:4px;
-}
-
-#footer-facebook {
-  background-image:url(http://www.braksoftware.com/codepen/bruce/v1/facebook-white-large.png); 
-}
-
-#footer-twitter {
-  background-image:url(http://www.braksoftware.com/codepen/bruce/v1/twitter-white-large.png);
-}
-
-#footer-blogspot {
-  background-image:url(http://www.braksoftware.com/codepen/bruce/v1/blogger-white-large.png);
+#footer-text {
+    display:block;
+    justify-content: center;
+    clear:both;
 }
 
 #footer-contact {
-  color:white;
+  color:rgb(10, 6, 6);
   font-size:12px;
   font-weight:300;
   padding:30px 0px 10px 0px;
+  text-align: center;
 }
 
 #footer-contact div {
@@ -166,15 +122,16 @@
 }
 
 #footer-copyright {
-  color:#eeeeee;
+  color:#2c2b2b;
   font-size:11px;
   font-weight:300;
   clear:both;
-  align-content:center;
+  text-align: center;
 }
 
-.footer-divider {
-  margin:0px 4px 0px 4px;
+#footer-divider {
+  margin: 4px;
+  color: #2c2b2b;
 }
 
 @media screen and (min-width:600px) and (max-width: 999px) {
@@ -194,47 +151,6 @@
     margin:0px 20px 0px 0px;
   }
   
-  #footer-logo {
-    background-image:url(http://www.braksoftware.com/codepen/bruce/v1/bruce-logo-footer-medium.png);
-    width:121px;
-    height:28px;
-    margin:4px 0px 37px 0px;
-  }
-  
-  #footer-join-our-team {
-    background-image:url(http://www.braksoftware.com/codepen/bruce/v1/join-our-team-button-medium.png);
-    width:120px;
-    height:27px;
-    margin:0px 0px 10px 0px;
-  }
-  
-  #footer-join-our-team:hover {
-  background-image:url(http://www.braksoftware.com/codepen/bruce/v1/join-our-team-button-over-medium.png);
-}
-  
-  .footer-social-media-link {
-    width:16px;
-    height:16px;
-    margin:4px;
-  }
-  
-  #footer-facebook {
-    background-image:url(http://www.braksoftware.com/codepen/bruce/v1/facebook-white-medium.png); 
-  }
-
-  #footer-twitter {
-    background-image:url(http://www.braksoftware.com/codepen/bruce/v1/twitter-white-medium.png);
-  }
-
-  #footer-blogspot {
-    background-image:url(http://www.braksoftware.com/codepen/bruce/v1/blogger-white-medium.png);
-  }
-  
-  #footer-contact {
-    font-size:13px;
-    font-weight:300;
-  }
-  
   #footer-contact div {
     /* So that the address and phone numbers are on separate lines */
     display:block;
@@ -246,11 +162,6 @@
     color:inherit;
     font-size:inherit;
     line-height:inherit;
-  }
-  
-  #footer-contact div:nth-last-child(2) {
-    /* So that there is no divider between the address and phone number lines */
-    display:none;
   }
 }
 
@@ -274,87 +185,6 @@
   .footer-group {
     width:300px;
     margin:0px 20px 20px 0px;
-  }
-  
-  .footer-group > * {
-    vertical-align:top;
-  }
-  
-  .footer-group-logo {
-    margin:0px 0px 20px 0px
-  }
-  
-  
-  #footer-logo {
-    background-image:url(http://www.braksoftware.com/codepen/bruce/v1/bruce-logo-footer-medium.png);
-    width:120px;
-    height:27px;
-    margin:0px 10px 20px 0px;
-  }
-  
-  #footer-join-our-team {
-    display:inline-block;
-    margin:0px 10px 10px 0px;
-  }
-  
-  .footer-social-media-link {
-    margin:8px 4px 0px 0px;
-  }
-  
-  #footer-contact {
-    font-size:14px;
-    line-height:24px;
-    padding:10px 0px 10px 0px;
-  }
-  
-  #footer-contact span.nw {
-    display:block;
-  }
-  
-  .footer-contact-company-name {
-    font-weight:400;
-  }
-  
-  .footer-divider {
-    display:none;
-  }
-
-  #footer-contact a {
-    display:inline;
-    color:inherit;
-    font-size:inherit;
-    line-height:inherit;
-  }
-}
-
-/* Retina graphics here */
-@media screen and (-webkit-min-device-pixel-ratio: 2) and (min-width:1000px) { 
-  #footer-logo {
-    background-image:url(http://www.braksoftware.com/codepen/bruce/v1/bruce-logo-footer-large@2x.png);
-  }
-  
-  #footer-join-our-team {
-    background-image:url(http://www.braksoftware.com/codepen/bruce/v1/join-our-team-button-large@2x.png);
-  }
-}
-
-@media screen and (-webkit-min-device-pixel-ratio: 2) and (min-width:600px) and (max-width: 999px) {
-  #footer-logo {
-    background-image:url(http://www.braksoftware.com/codepen/bruce/v1/bruce-logo-footer-medium@2x.png);
-  }
-  
-  #footer-join-our-team {
-    background-image:url(http://www.braksoftware.com/codepen/bruce/v1/join-our-team-button-medium@2x.png);
-  }
-}
-
-@media screen and (-webkit-min-device-pixel-ratio: 2) and (max-width: 599px) {
-  #footer-logo {
-    background-image:url(http://www.braksoftware.com/codepen/bruce/v1/bruce-logo-footer-medium@2x.png);
-  }
-  
-  #footer-join-our-team {
-    background-image:url(http://www.braksoftware.com/codepen/bruce/v1/join-our-team-button-large@2x.png);
   }
 }
 
