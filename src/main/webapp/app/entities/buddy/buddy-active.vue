5eb68b75c6f3fbe233ca1754011f4375b5059b05
jcbackenddev <jcbackenddev@gmail.com>
2021-01-22 10:56:09 +0100

    Creation of the front page for the buddy active
    in order to have only one account being shown to the user, we had to
    create a page that display the information of the connected user as he
    goes to the settings of his account. That was harder than expected!

---
+++ src/main/webapp/app/entities/buddy/buddy-active.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-8">
+            <div v-if="buddy">
+                <h2 class="jh-entity-heading"><span>Buddy</span> {{buddy.id}}</h2>
+                <dl class="row jh-entity-details">
+                    <dt>
+                        <span>Lala Name</span>
+                    </dt>
+                    <dd>
+                        <span>{{buddy.firstName}}</span>
+                    </dd>
+                    <dt>
+                        <span>Last Name</span>
+                    </dt>
+                    <dd>
+                        <span>{{buddy.lastName}}</span>
+                    </dd>
+                    <dt>
+                        <span>Email</span>
+                    </dt>
+                    <dd>
+                        <span>{{buddy.email}}</span>
+                    </dd>
+                    <dt>
+                        <span>Phone Number</span>
+                    </dt>
+                    <dd>
+                        <span>{{buddy.phoneNumber}}</span>
+                    </dd>
+                    <dt>
+                        <span>Balance</span>
+                    </dt>
+                    <dd>
+                        <span>{{buddy.balance}}</span>
+                    </dd>
+                    <dt>
+                        <span>User</span>
+                    </dt>
+                    <dd>
+                        {{buddy.user ? buddy.user.id : ''}}
+                    </dd>
+                    <dt>
+                        <span>Bank Account</span>
+                    </dt>
+                    <dd>
+                        <div v-if="buddy.bankAccount">
+                            <router-link :to="{name: 'BankAccountView', params: {bankAccountId: buddy.bankAccount.id}}">{{buddy.bankAccount.id}}</router-link>
+                        </div>
+                    </dd>
+                </dl>
+                <button type="submit"
+                        v-on:click.prevent="previousState()"
+                        class="btn btn-info">
+                    <font-awesome-icon icon="arrow-left"></font-awesome-icon>&nbsp;<span> Back</span>
+                </button>
+                <router-link v-if="buddy.id" :to="{name: 'BuddyEdit', params: {buddyId: buddy.id}}" tag="button" class="btn btn-primary">
+                    <font-awesome-icon icon="pencil-alt"></font-awesome-icon>&nbsp;<span> Edit</span>
+                </router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" src="./buddy-active.component.ts">
+</script>
