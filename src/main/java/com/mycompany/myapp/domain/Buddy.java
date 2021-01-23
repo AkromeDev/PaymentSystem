c61e244738f04bfbc749c1e48630bdd584b2fff2
jcbackenddev <jcbackenddev@gmail.com>
2021-01-23 21:16:41 +0100

    auto generation of the buddyId desactivated

--- src/main/java/com/mycompany/myapp/domain/Buddy.java
+++ src/main/java/com/mycompany/myapp/domain/Buddy.java
@@ -24,7 +24,6 @@ public class Buddy implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
     @SequenceGenerator(name = "sequenceGenerator")
     private Long id;
 
