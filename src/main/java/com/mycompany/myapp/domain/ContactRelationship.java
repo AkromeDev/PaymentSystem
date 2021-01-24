c55567ec37e3337593b81c01bc6955eb34282171
jcbackenddev <jcbackenddev@gmail.com>
2021-01-24 11:13:49 +0100

    auto generation of the ID cancelled

--- src/main/java/com/mycompany/myapp/domain/ContactRelationship.java
+++ src/main/java/com/mycompany/myapp/domain/ContactRelationship.java
@@ -19,7 +19,6 @@ public class ContactRelationship implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
     @SequenceGenerator(name = "sequenceGenerator")
     private Long id;
 
