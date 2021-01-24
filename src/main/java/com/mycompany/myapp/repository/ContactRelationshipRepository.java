378d0f05c3fbb5becbd85421ccb251c707aeb332
jcbackenddev <jcbackenddev@gmail.com>
2021-01-24 11:23:30 +0100

    New Query for the new endpoint: /contact-relationships

--- src/main/java/com/mycompany/myapp/repository/ContactRelationshipRepository.java
+++ src/main/java/com/mycompany/myapp/repository/ContactRelationshipRepository.java
@@ -1,14 +1,22 @@
 package com.mycompany.myapp.repository;
 
+import com.mycompany.myapp.domain.Buddy;
 import com.mycompany.myapp.domain.ContactRelationship;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
  * Spring Data  repository for the ContactRelationship entity.
  */
-@SuppressWarnings("unused")
 @Repository
 public interface ContactRelationshipRepository extends JpaRepository<ContactRelationship, Long> {
+
+	@Query(value = "select CONTACT_RELATIONSHIP.* from CONTACT_RELATIONSHIP CONTACT_RELATIONSHIP where CONTACT_RELATIONSHIP.id=:id", nativeQuery = true)
+	List<ContactRelationship> findAllFromUser(@Param("id")Long id);
+	
 }
