6da89bd68e7d475c48cfb00020dfaec34f3b1500
jcbackenddev <jcbackenddev@gmail.com>
2021-02-03 10:53:17 +0100

    ID userid1 userid2 deleted
    The user only needs to input the email in order to add a contact!

--- src/main/webapp/app/entities/contact-relationship/contact-relationship-update.vue
+++ src/main/webapp/app/entities/contact-relationship/contact-relationship-update.vue
@@ -4,27 +4,10 @@
             <form name="editForm" role="form" novalidate v-on:submit.prevent="save()" >
                 <h2 id="paymentSystemApp.contactRelationship.home.createOrEditLabel">Create or edit a ContactRelationship</h2>
                 <div>
-                    <div class="form-group" v-if="contactRelationship.id">
-                        <label for="id">ID</label>
-                        <input type="text" class="form-control" id="id" name="id"
-                               v-model="contactRelationship.id" readonly />
-                    </div>
                     <div class="form-group">
-                        <label class="form-control-label" for="contact-relationship-userId1">User Id 1</label>
-                        <input type="number" class="form-control" name="userId1" id="contact-relationship-userId1"
-                            :class="{'valid': !$v.contactRelationship.userId1.$invalid, 'invalid': $v.contactRelationship.userId1.$invalid }" v-model.number="$v.contactRelationship.userId1.$model" />
-                    </div>
-                    <div class="form-group">
-                        <label class="form-control-label" for="contact-relationship-userId2">User Id 2</label>
-                        <input type="number" class="form-control" name="userId2" id="contact-relationship-userId2"
-                            :class="{'valid': !$v.contactRelationship.userId2.$invalid, 'invalid': $v.contactRelationship.userId2.$invalid }" v-model.number="$v.contactRelationship.userId2.$model" />
-                    </div>
-                    <div class="form-group">
-                        <label class="form-control-label" for="contact-relationship-buddy">Buddy</label>
-                        <select class="form-control" id="contact-relationship-buddy" name="buddy" v-model="contactRelationship.buddy">
-                            <option v-bind:value="null"></option>
-                            <option v-bind:value="contactRelationship.buddy && buddyOption.id === contactRelationship.buddy.id ? contactRelationship.buddy : buddyOption" v-for="buddyOption in buddies" :key="buddyOption.id">{{buddyOption.id}}</option>
-                        </select>
+                        <label class="form-control-label" for="contact-relationship-email">Email</label>
+                        <input type="text" class="form-control" name="email" id="contact-relationship-email"
+                            :class="{'valid': !$v.contactRelationship.email.$invalid, 'invalid': $v.contactRelationship.email.$invalid }" v-model="$v.contactRelationship.email.$model" />
                     </div>
                 </div>
                 <div>
